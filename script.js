caf66f08919b865263d5270308ab5e52746474d2
Giovanna <giovannad1clonius@gmail.com>
2023-01-11 02:58:22 -0300

    Função quantidade de cartas adicionada

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// const qtddeCartas = prompt("Insira a quantidade de cartas:");
+qtdadeCartas();
+let quantidadeCartas;
+const cartas = [];
+
+function qtdadeCartas(){
+    let quantidade = Number(prompt("Insira a quantidade de cartas (4 a 14):"));
+
+    while (quantidade % 2 === 1 || quantidade < 4 || quantidade > 14){ 
+       quantidade = Number(prompt("Insira a quantidade de cartas (4 a 14):")); 
+    } 
+    
+    quantidadeCartas.push(quantidade);
+}
+
+/*inserir as cartas viradas pra baixo na página de forma que a distribuição 
+seja aleatória.*/
